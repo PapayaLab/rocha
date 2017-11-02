e2cf0979b10afdcdaab16069003a9a58f6a03291
pipiyo <fe.cavieres@gmail.com>
2017-11-02 15:55:02 -0300

    nada

--- source/client/components/login/LoginForm.jsx
+++ source/client/components/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import s from './LoginForm.css';
 
 //console.log(s[0][1]);
@@ -13,6 +13,7 @@ const LoginForm = props => (
     {console.log(props)}
     {props.loading && <h1>loading</h1>}
     <h1 className={s.titulo}>Login</h1>
+    <Link to="/dashboard">Back to home</Link>
     <input
       type="text"
     />
