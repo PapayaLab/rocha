2e96cf7b7efc6e2ebe475e79e66c45d575cef60f
pipiyo <fe.cavieres@gmail.com>
2017-11-09 11:57:03 -0300

    coneccion con redux

--- source/client/reducers/index.js
+++ source/client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import singUp from './singUp';
+import saveCotizacion from './dashboard/dam/save_cotizacion/saveCotizacion';
 
 const reducer = combineReducers({
   singUp,
+  saveCotizacion,
 });
 
 export default reducer;
